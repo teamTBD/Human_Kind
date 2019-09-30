5f8eddd307a9ed79cf774b3a3aec455c61c11b02
rosborn92 <rosborn92@gmail.com>
2019-09-30 10:08:53 -0700

    adds sign in/out button to nav and jumbotron

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -1,40 +1,66 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Nav, NavLink, NavItem } from 'reactstrap'
+import { Nav, NavLink, NavItem, Jumbotron } from 'reactstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Feed from './pages/Feed'
 
 
 class MainApp extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            deeds: [],
+            logged_in: ""
+        }
+
+    }
+
+
+
+
+
+
+
     render () {
       const {
+
         logged_in,
         sign_in_route,
         sign_out_route
       } = this.props
 
-
-
     return (
       <React.Fragment>
         <Router>
-          {logged_in &&
-            <div>
-              <a href={sign_out_route}>Sign Out</a>
-            </div>
-          }
-          {!logged_in &&
-            <div>
-              <a href={sign_in_route}>Sign In</a>
-            </div>
-          }
+
 
           <Nav>
+            {logged_in &&
             <NavItem>
                 <NavLink href="/deed_feed">Deed Feed</NavLink>
             </NavItem>
+            }
+            {logged_in &&
+            <NavItem>
+                <NavLink href={sign_out_route}>Sign Out</NavLink>
+            </NavItem>
+            }
+            {!logged_in &&
+            <NavItem>
+                <NavLink href={sign_in_route}>Sign In</NavLink>
+            </NavItem>
+            }
           </Nav>
+
+          <Jumbotron>
+            <h1>
+                Human Kind
+            </h1>
+            <p>
+                a site for sharing good deeds in your community and connecting with others to deed the needy
+            </p>
+          </Jumbotron>
 
           <Route
             exact

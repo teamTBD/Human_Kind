fa7377e970d8dde193cfed48c24ab7f31902085b
rosborn92 <rosborn92@gmail.com>
2019-10-01 10:54:49 -0700

    edits posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,29 @@ class PostsController < ApplicationController
         render json: post, status:201
     end
 
+    def show
+        post = Post.find(params[:id])
+        render json: post
+    end
 
+    def update
+        post = current_user.posts.find(params[:id])
+        if post.update(title: params[:title], description:      params[:description], location: params[:location])
+            post.save
+            render json: post
+        else
+            render json: {error: 'could not update'}, status: 400
+        end
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+    end
 
     private
     def post_params
         params.require(:post).permit(:title, :description, :location)
-    end 
+    end
 
 end

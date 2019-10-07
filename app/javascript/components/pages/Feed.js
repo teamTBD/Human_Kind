39c3dda5e3c9ad0615ce4199d490d2c389936f58
Ubuntu <ubuntu@ip-172-31-30-115.us-east-2.compute.internal>
2019-10-07 21:58:29 +0000

    indentation fixes

--- app/javascript/components/pages/Feed.js
+++ app/javascript/components/pages/Feed.js
@@ -1,86 +1,85 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Nav, Button, ModalHeader, ModalBody, ModalFooter, Modal} from 'reactstrap'
+import { Nav, Button, ModalHeader, ModalBody, ModalFooter, Modal } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 
 class Feed extends React.Component {
-      constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
-
-    this.toggle = this.toggle.bind(this);
-  }
-  
-  
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-    
-
-    componentDidMount() {
-        this.props.changeSuccess(false)
+    constructor(props) {
+        super(props);
+            this.state = {
+             modal: false
+            };
+            this.toggle = this.toggle.bind(this);
     }
 
-    render () {
-        const {
-            posts,
-            handleDeletePost,
-            current_user_id
-        } = this.props
-    return (
-      <React.Fragment>
-      
-        <h1>Deed Feed</h1>
- 
-        <div>
-            {posts.map((post) => {
-                return(
-                    <div key={post.id}>
-                        {post.title}
-                        {post.description}
-                        {post.location}
-                        {post.user_id === current_user_id &&
-                            <div>
-                            <Button>
-                                <Link to={`/edit_post/${post.id}`}>Edit Post
-                                </Link>
-                            </Button>
-                            <Button onClick={() => window.confirm("Are you sure you wish to delete post?") && this.props.handleDeletePost(post.id)}>Delete Post</Button>
-                            
-                            <Button color='primary' onClick={this.toggle}> Expand {this.props.buttonLabel}</Button>
-                                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                                  <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                                  <ModalBody>
-                                  {post.title}
-                                  {post.description}
-                                  {post.location}
-                                  </ModalBody>
-                                  <ModalFooter>
-                                    <Button color='info' onClick={this.toggle}>Visit Profile</Button>{' '}
-                                    <Button color='success' onClick={this.toggle}>Like</Button>
-                                  </ModalFooter>
-                                </Modal>
-                            </div>
-                        }
-                    </div>
-                    
-                    
-                    
-                )
-            })}
-            
-            
-        </div>
-        
 
-      </React.Fragment>
+toggle() {
+    this.setState({
+     modal: !this.state.modal
+     });
+    }
+
+
+componentDidMount() {
+    this.props.changeSuccess(false)
+    }
+
+render() {
+    const {
+        posts,
+        handleDeletePost,
+        current_user_id
+    } = this.props
+    return (
+        <React.Fragment>
+  
+<h1>Deed Feed</h1>
+
+<div>
+    {posts.map((post) => {
+        return(
+            <div key={post.id}>
+                {post.title}
+                {post.description}
+                {post.location}
+                {post.user_id === current_user_id &&
+                 <div>
+            <Button>
+                <Link to={`/edit_post/${post.id}`}>Edit Post
+                </Link>
+            </Button>
+            <Button onClick={() => window.confirm("Are you sure you wish to delete post?") && this.props.handleDeletePost(post.id)}>Delete Post</Button>
+            
+            <Button color='primary' onClick={this.toggle}> Expand {this.props.buttonLabel}</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                  <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                  <ModalBody>
+                  {post.title}
+                  {post.description}
+                  {post.location}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color='info' onClick={this.toggle}>Visit Profile</Button>{' '}
+                    <Button color='success' onClick={this.toggle}>Like</Button>
+                  </ModalFooter>
+                </Modal>
+            </div>
+            }
+            </div>
+            
+            
+                
+            )
+        })}
+        
+        
+</div>
+    
+
+</React.Fragment>
     );
-  }
+}
 }
 
 export default Feed

ba7965606e82952c4989630b34f268c03de55564
Ubuntu <ubuntu@ip-172-31-24-40.us-east-2.compute.internal>
2019-10-07 20:02:31 +0000

    cleaned up code for Feed, NewDeed

--- app/javascript/components/pages/Feed.js
+++ app/javascript/components/pages/Feed.js
@@ -19,47 +19,47 @@ class Feed extends React.Component {
     return (
         
         
-      <React.Fragment >
-       
-       
-        <div style={{textAlign: 'center'}}>
-         <h1>Deed Feed</h1>
-         <ListGroup style={{marginLeft:'25rem', marginRight:'25rem', marginTop:'5rem', marginBottom:'5rem'}}>
-        
+    <React.Fragment >
+     <div style={{textAlign: 'center'}}>
+      <h1>Deed Feed</h1>
+       <ListGroup style={{marginLeft:'25rem', marginRight:'25rem', marginTop:'5rem', marginBottom:'5rem'}}>
             {posts.map((post) => {
                 return(
-               <ListGroupItem> 
-                    <div key={post.id}>
- <ListGroupItemHeading>  {post.title} </ListGroupItemHeading>
-                 <ListGroupItemText>  
-                  {post.description}
+                 <ListGroupItem> 
+                  <div key={post.id}>
+                    <ListGroupItemHeading>  {post.title} </ListGroupItemHeading>
+                     <ListGroupItemText>  
+                        {post.description}
                         {post.location}
                         {post.user_id === current_user_id &&
-                            <div>
-                
-                            <Button outline color="info" size="sm">
+                           <div>
+                             <Button outline color="info" size="sm">
                                 <Link to={`/edit_post/${post.id}`}>Edit Post
                                 </Link>
-                            </Button>
-                            <Button outline color="danger" size="sm" onClick={() => window.confirm("Are you sure you wish to delete post?") && this.props.handleDeletePost(post.id)}>Delete Post</Button>
-                            </div> 
-                        }
-                          
-                        </ListGroupItemText>
-                        
-                    </div>
-                    
-                   </ListGroupItem>
+                             </Button>
+                             <Button outline color="danger" size="sm" onClick={() => window.confirm("Are you sure you wish to delete post?") && this.props.handleDeletePost(post.id)}>Delete Post</Button>
+                           </div> 
+                            }
+                     </ListGroupItemText>
+                  </div>
+                 </ListGroupItem>
                 )
             })}
-          
-            </ListGroup>
-            
-        </div>
-       
-      </React.Fragment>
+       </ListGroup>
+      </div>
+    </React.Fragment>
     );
   }
 }
+            
+                        
+          
+       
+                
+                          
+                    
+       
+        
+       
 
 export default Feed

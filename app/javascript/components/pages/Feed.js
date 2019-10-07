5be1c1babcd94963023b6a5437dfe9963ce85ba9
brianlin <brianlin96@yahoo.com>
2019-10-07 11:37:45 -0700

    added title and description from posts to marker popup

--- app/javascript/components/pages/Feed.js
+++ app/javascript/components/pages/Feed.js
@@ -10,18 +10,18 @@ class Feed extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+          //hard-coded center of map
           lat: 32.5732878512982,
           lng: -117.107984521922,
           zoom: 12,
+          //needs to be a number as an initial value
           markerLat: 0,
           markerLng: 0, 
           markerPosition: [0,0],
           markers: []
-        }
-        console.log(props.posts);
-        
+        }   
       }
-    
+    //need this to load markers when switching between components
     componentDidMount() {
         const { posts } = this.props
         this.props.changeSuccess(false)
@@ -30,23 +30,39 @@ class Feed extends React.Component {
             provider.search({ query: post.location })
             .then((result) => { 
                 this.setState({markerLat:result[0].y, markerLng:result[0].x})
-                this.setState({markers: [...this.state.markers, [this.state.markerLat, this.state.markerLng]]})
+                let title = post.title
+                let description = post.description
+                let marker = {
+                    location: [this.state.markerLat, this.state.markerLng], 
+                    title: title, 
+                    description: description
+                }
+                this.setState({markers: [...this.state.markers, marker ]})
             })
         })
     }
-
-    componentDidUpdate(prevProps ) {
+    //used to load markers when the state of posts is changed
+    componentDidUpdate(prevProps) {
         const { posts } = this.props
         if(prevProps.posts != posts ){
-        
+            //used to reset state to rerender component
+            this.setState({markers: []})
             const provider = new OpenStreetMapProvider();
             posts.map((post)=>{
                 provider.search({ query: post.location })
                 .then((result) => {
                     this.setState({markerLat:result[0].y, markerLng:result[0].x})
-                    this.setState({markers: [...this.state.markers, [this.state.markerLat, this.state.markerLng]]})
+                    let title = post.title
+                    let description = post.description
+                    let marker = {
+                        location: [this.state.markerLat, this.state.markerLng], 
+                        title: title, 
+                        description: description
+                    }
+                    this.setState({markers: [...this.state.markers, marker ]})
                 })
             })
+            
         }
     }
         
@@ -65,9 +81,9 @@ class Feed extends React.Component {
                 
                 {markers.map((marker ,index)=>{
                     return(
-                        <Marker key= {index} position={marker} draggable={true}>
+                        <Marker key= {index} position={marker.location} draggable={true}>
                             <Popup>
-                                A pretty CSS3 popup. <br/> Easily customizable.
+                                Title: {marker.title} <br/> Description: {marker.description}
                             </Popup>
                         </Marker>
                     ) 

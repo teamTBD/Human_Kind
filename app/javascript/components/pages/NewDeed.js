cbee3796c0ff129aa4704f8a8d3f00513f4969e3
rosborn92 <rosborn92@gmail.com>
2019-10-09 14:33:33 -0700

    removes image validation from post model

--- app/javascript/components/pages/NewDeed.js
+++ app/javascript/components/pages/NewDeed.js
@@ -62,7 +62,7 @@ class NewDeed extends React.Component {
           <FormGroup style={{width:"50%", textAlign:'center'}}>
             <Label style={{color:'white'}}>Description of Your Deed</Label>
                 <Input
-                    type='textarea'
+                    type='text'
                     name="description"
                     value={description}
                     rows='3'
@@ -75,15 +75,6 @@ class NewDeed extends React.Component {
         </center>
     </Container>
   </React.Fragment>
-
-
-
-
-
-
-
-
-
 
     );
   }

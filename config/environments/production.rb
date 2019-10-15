6e45422d1cf3902a21fafdca65ecc76f189be67d
rosborn92 <rosborn92@gmail.com>
2019-10-15 14:22:41 -0700

    heroku development mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "cryptic-crag-66013.herokuapp.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
